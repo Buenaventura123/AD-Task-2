4ecf506cdd3510a0af8ad89a61578049917c3233
Buenaventura123 <202310536@fit.edu.ph>
2025-05-18 15:40:26 +0800

    refactor: Change the style of the navbar

--- components/navbar.component.php
+++ components/navbar.component.php
@@ -1,15 +1,21 @@
+
 <?php
 function createNavbar($active = '') {
     $base = '/AD-Task-2';
 
     return '
-    <nav class="navbar">
-        <ul>
+   <header>
+        <nav class="navbar">
+            <a href="' . $base . '/page/home/index.php" class="logo"; class="' . ($active === "All Movies" ? "active" : "") . '">MOVIE RATES<span>.</span></a>
+            <ul class="menu-links">
+                <span id="close-menu-btn" class="material-symbols-outlined">close</span>
             <li><a href="' . $base . '/page/home/index.php" class="' . ($active === "All Movies" ? "active" : "") . '">Home</a></li>
             <li><a href="' . $base . '/page/high-rated/index.php" class="' . ($active === "High Rated" ? "active" : "") . '">High Rated</a></li>
             <li><a href="' . $base . '/page/low-rated/index.php" class="' . ($active === "Low Rated" ? "active" : "") . '">Low Rated</a></li>
-        </ul>
-    </nav>
+            </ul>
+            <span id="menu-btn" class="material-symbols-outlined">menu</span>
+        </nav>
+    </header>
     ';
 }
 ?>
