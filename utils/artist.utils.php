4f4438393a0a3d7aaeb3d7c2e03c126ecfeb533d
Buenaventura123 <202310536@fit.edu.ph>
2025-06-01 22:52:14 +0800

    style: added new images

--- utils/artist.utils.php
+++ utils/artist.utils.php
@@ -3,73 +3,73 @@
 $topArtists = [
     [
         "name" => "Isabela Merced",
-        "image" => "../../assets/images/artists/isabela.jpg",
+        "image" => "../../assets/img/artists/isabela.jpg",
         "rank" => 1,
         "change" => "↔"
     ],
     [
         "name" => "Lewis Pullman",
-        "image" => "../../assets/images/artists/lewis.jpg",
+        "image" => "../../assets/img/artists/lewis.jpg",
         "rank" => 2,
         "change" => "+4"
     ],
     [
         "name" => "Florence Pugh",
-        "image" => "../../assets/images/artists/florence.jpg",
+        "image" => "../../assets/img/artists/florence.jpg",
         "rank" => 3,
         "change" => "+1"
     ],
     [
         "name" => "Erika Henningsen",
-        "image" => "../../assets/images/artists/erika.jpg",
+        "image" => "../../assets/img/artists/erika.jpg",
         "rank" => 4,
         "change" => "+14"
     ],
     [
         "name" => "Timothée Chalamet",
-        "image" => "../../assets/images/artists/timothee.jpg",
+        "image" => "../../assets/img/artists/timothee.jpg",
         "rank" => 5,
         "change" => "+2"
     ],
     [
         "name" => "Zendaya",
-        "image" => "../../assets/images/artists/zendaya.jpg",
+        "image" => "../../assets/img/artists/zendaya.jpg",
         "rank" => 6,
         "change" => "-1"
     ],
     [
         "name" => "Austin Butler",
-        "image" => "../../assets/images/artists/austin.jpg",
+        "image" => "../../assets/img/artists/austin.jpg",
         "rank" => 7,
         "change" => "↔"
     ],
     [
         "name" => "Sydney Sweeney",
-        "image" => "../../assets/images/artists/sydney.jpg",
+        "image" => "../../assets/img/artists/sydney.jpg",
         "rank" => 8,
         "change" => "+3"
     ],
     [
         "name" => "Jenna Ortega",
-        "image" => "../../assets/images/artists/jenna.jpg", // Placeholder, you'll need to create this image
+        "image" => "../../assets/img/artists/jenna.jpg", // Placeholder, you'll need to create this image
         "rank" => 9,
         "change" => "+5"
     ],
     [
         "name" => "Jacob Elordi",
-        "image" => "../../assets/images/artists/jacob.jpg", // Placeholder, you'll need to create this image
+        "image" => "../../assets/img/artists/jacob.jpg", // Placeholder, you'll need to create this image
         "rank" => 10,
         "change" => "+2"
     ],
     [
         "name" => "Margot Robbie",
-        "image" => "../../assets/images/artists/margot.jpg", // Placeholder, you'll need to create this image
+        "image" => "../../assets/img/artists/margot.jpg", // Placeholder, you'll need to create this image
         "rank" => 11,
         "change" => "-3"
     ],
     [
         "name" => "Ryan Gosling",
-        "image" => "../../assets/images/artists/ryan.jpg", // Placeholder, you'll need to create this image
+        "image" => "../../assets/img/artists/ryan.jpg", // Placeholder, you'll need to create this image
         "rank" => 12,
         "change" => "+1"
     ]
