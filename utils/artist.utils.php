3fc7b8fb179c183b4d698671e5385c32c21d7a31
Buenaventura123 <202310536@fit.edu.ph>
2025-06-01 22:27:17 +0800

    feat: added more artist

--- utils/artist.utils.php
+++ utils/artist.utils.php
@@ -24,7 +24,55 @@ $topArtists = [
         "image" => "../../assets/images/artists/erika.jpg",
         "rank" => 4,
         "change" => "+14"
+    ],
+    [
+        "name" => "Timothée Chalamet",
+        "image" => "../../assets/images/artists/timothee.jpg",
+        "rank" => 5,
+        "change" => "+2"
+    ],
+    [
+        "name" => "Zendaya",
+        "image" => "../../assets/images/artists/zendaya.jpg",
+        "rank" => 6,
+        "change" => "-1"
+    ],
+    [
+        "name" => "Austin Butler",
+        "image" => "../../assets/images/artists/austin.jpg",
+        "rank" => 7,
+        "change" => "↔"
+    ],
+    [
+        "name" => "Sydney Sweeney",
+        "image" => "../../assets/images/artists/sydney.jpg",
+        "rank" => 8,
+        "change" => "+3"
+    ],
+    [
+        "name" => "Jenna Ortega",
+        "image" => "../../assets/images/artists/jenna.jpg", // Placeholder, you'll need to create this image
+        "rank" => 9,
+        "change" => "+5"
+    ],
+    [
+        "name" => "Jacob Elordi",
+        "image" => "../../assets/images/artists/jacob.jpg", // Placeholder, you'll need to create this image
+        "rank" => 10,
+        "change" => "+2"
+    ],
+    [
+        "name" => "Margot Robbie",
+        "image" => "../../assets/images/artists/margot.jpg", // Placeholder, you'll need to create this image
+        "rank" => 11,
+        "change" => "-3"
+    ],
+    [
+        "name" => "Ryan Gosling",
+        "image" => "../../assets/images/artists/ryan.jpg", // Placeholder, you'll need to create this image
+        "rank" => 12,
+        "change" => "+1"
     ]
 ];
 
-
+?>
